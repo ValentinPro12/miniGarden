8e1fef5116e258836da42b3884af5e41cea3729f
valentin <v.prokhorov@telecontact.ru>
2022-04-17 23:20:42 +0300

    рефпкторинг + начал делать предложения

--- main/models.py
+++ main/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 
 
-
-
-
 class Gardens(models.Model):
     title = models.CharField(max_length=50, verbose_name='Тип сада')
     age = models.CharField(max_length=15, verbose_name='Возрост от')
+    time = models.CharField(max_length=30,  blank=True, null=True, verbose_name='Время с по ')
     description = models.TextField(null=True, blank=True, verbose_name='Описание')
     price_for_one = models.FloatField(null=True, blank=True, verbose_name='Цена за одно занятие')
     price_for_month = models.FloatField(null=True, blank=True, verbose_name='Цена за месяц')
     types = models.ManyToManyField('Schedule', related_name='type')
+    poster = models.ImageField("Постер", blank=True, upload_to="posters")
 
     def __str__(self):
         return self.title
